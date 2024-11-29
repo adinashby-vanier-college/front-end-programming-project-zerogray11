9af8e5314e97fd8ef2e1b3d3902a6ea40c942223
Zero <zerogray11@gmail.com>
2024-11-29 12:35:26 -0500

    sorry I forgot to commit

---
+++ Protfolio.js
@@ -0,0 +1,11 @@
+const scrollBox= document.getElementById('scrollBox');
+
+function checkScroll(){
+    if(window.scrollY>200){
+        scrollBox.classList.add('show');
+
+    }else{
+        scrollBox.classList.remove('show');
+    }
+}
+window.addEventListener('scroll',checkScroll);
