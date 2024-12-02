f56a337cc92021ff54311796d6ae0de344d9e7b2
Zero <zerogray11@gmail.com>
2024-12-02 15:45:06 -0500

    "javascript function for smoother interactive hover over buttons, change of background pic in boxContact and in addition to other design and style adjustments"

--- Protfolio.js
+++ Protfolio.js
@@ -9,3 +9,15 @@ function checkScroll(){
     }
 }
 window.addEventListener('scroll',checkScroll);
+
+document.querySelectorAll('.javascript, .java, .sql, .html, .css, .nodejs, .aws, .photoshop, .polisci, .compsci, .cloud, .sendmessage').forEach(button => {
+    button.addEventListener('mouseenter', () => {
+        button.style.transform = 'scale(1.1)';
+        button.style.transition = 'transform 0.3s ease-in-out';
+    });
+    button.addEventListener('mouseleave', () => {
+        button.style.transform = 'scale(1)';
+    });
+});
+
+
